dd0b650d50de70df0eeb83995ddf6af7d463dad0
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-11-13 18:40:12 +0300

    finished task

--- main.dart
+++ main.dart
@@ -1,10 +1,21 @@
+class Movie {
+  final String name;
+  final String genre;
+  List<int> ratings;
+
+  Movie({required this.name, required this.genre, required this.ratings});
+
+  void printDetails() {
+    print(this.name);
+    print(this.genre);
+    print(this.ratings);
+  }
+}
+
 void main() {
-  final movie = {
-    'name': 'The Amazing SpiderMan',
-    'genre': 'Action',
-    'ratings': [1.5,3.2,6.4]
-  };
-  print(movie['name']);
-  print(movie['genre']);
-  print(movie['ratings']);
+  var movie1 = Movie(name: "Braveheart", genre: "Drama", ratings: [5, 5]);
+  var movie2 =
+      Movie(name: "The Godfather", genre: "Drama", ratings: [5, 5, 5, 5]);
+  movie1.printDetails();
+  movie2.printDetails();
 }
